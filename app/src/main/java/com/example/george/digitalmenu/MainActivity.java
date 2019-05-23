518797114535a3f25a8512c8a88bac090f1c84ea
PereteanuGeorge <gp3917@ic.ac.uk>
2019-05-23 12:31:03 +0100

    Added functionality for checking the network connectivity.

--- app/src/main/java/com/example/george/digitalmenu/MainActivity.java
+++ app/src/main/java/com/example/george/digitalmenu/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.george.digitalmenu;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkIfNetworkIsAvailable();
+    }
+
+    private void checkIfNetworkIsAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        if (!(activeNetworkInfo != null && activeNetworkInfo.isConnected())) {
+            context = getApplicationContext();
+            Toast toast = Toast.makeText(context, "Please connect to the internet", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
