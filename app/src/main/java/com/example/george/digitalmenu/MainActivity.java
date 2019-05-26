97c001d59ec2799752776c2b6b0e697e66276fb3
hay-dee <null>
2019-05-26 16:57:33 +0100

    Dynamic adding of menu item cards

--- app/src/main/java/com/example/george/digitalmenu/MainActivity.java
+++ app/src/main/java/com/example/george/digitalmenu/MainActivity.java
@@ -5,8 +5,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,12 +27,23 @@ public class MainActivity extends AppCompatActivity {
     private RestaurantDatabase db;
     private String TAG = "MainActivity";
 
+
+    private CardView previousCard;
+    private ConstraintLayout rootLayout;
+    private ConstraintSet constraintSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         checkIfNetworkIsAvailable();
+
+        rootLayout = findViewById(R.id.rootLayout);
+        constraintSet = new ConstraintSet();
+
+        addMenuItem();
+        addMenuItem();
 
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
         mAuth.signInAnonymously()
@@ -49,6 +66,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+    }
+
+    private void addMenuItem() {
+        LayoutInflater inflater = getLayoutInflater();
+        CardView card = (CardView) inflater.inflate(R.layout.menu_item_card, rootLayout, false);
+        card.setId(View.generateViewId());
+        rootLayout.addView(card);
+
+        if (previousCard != null) {
+            constraintSet.clone(rootLayout);
+            constraintSet.connect(card.getId(), ConstraintSet.TOP, previousCard.getId(), ConstraintSet.BOTTOM);
+            constraintSet.applyTo(rootLayout);
+        }
+
+        previousCard = card;
     }
 
     private void checkIfNetworkIsAvailable() {
