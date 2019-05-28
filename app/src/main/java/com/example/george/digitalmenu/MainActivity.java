1acce94d9359884ca154da83fc43269e036321a3
Yoram Boccia <yb4817@ac.ic.uk>
2019-05-28 15:53:18 +0100

    Implemented design for dish_info

--- app/src/main/java/com/example/george/digitalmenu/MainActivity.java
+++ app/src/main/java/com/example/george/digitalmenu/MainActivity.java
@@ -2,12 +2,8 @@ package com.example.george.digitalmenu;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.view.SurfaceView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         new Network().checkIfNetworkIsAvailable(this);
 
         SurfaceView surfaceView = findViewById(R.id.camerapreview);
