a5dc53123dc989e357c013e85958991cd9cdc66c
PereteanuGeorge <gp3917@ic.ac.uk>
2019-05-30 16:08:43 +0100

    Refactored code

--- app/src/main/java/com/example/george/digitalmenu/MainActivity.java
+++ app/src/main/java/com/example/george/digitalmenu/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.george.digitalmenu;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
-
-import android.support.v4.util.Consumer;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
@@ -49,7 +45,6 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         if (!(activeNetworkInfo != null && activeNetworkInfo.isConnected())) {
-//            Context context = getApplicationContext();
             Toast toast = Toast.makeText(context, "Please connect to the internet", Toast.LENGTH_SHORT);
             toast.show();
         }
