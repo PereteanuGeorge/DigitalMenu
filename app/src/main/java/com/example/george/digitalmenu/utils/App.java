62dfe09f3d79c781db2738c4c965f01f11cc36fe
PereteanuGeorge <gp3917@ic.ac.uk>
2019-06-03 15:32:23 +0100

    Fixed open activity error on main screen

--- app/src/main/java/com/example/george/digitalmenu/utils/App.java
+++ app/src/main/java/com/example/george/digitalmenu/utils/App.java
@@ -16,6 +16,7 @@ public class App extends Application {
         initServiceRegistry();
 
         Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
